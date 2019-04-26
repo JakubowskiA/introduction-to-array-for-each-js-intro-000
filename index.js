42376ac95b53cf320a98b67e81ca76b35444ad59
Ariel J <arieljakubowski@gmail.com>
2019-04-26 16:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(array, callback) {
+function doToElementsInArray(array) {
   array.forEach(array => {
     return `Mmmm, ${array}!!!`;
   });
